ead5a1d57c866d72644744edae9ec16a8646db67
Documenter.jl <documenter@juliadocs.github.io>
2022-05-29 20:59:06 +0000

    build based on 404c6a5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MLJCompose","category":"page"},{"location":"#MLJCompose","page":"Home","title":"MLJCompose","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MLJCompose.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MLJCompose]","category":"page"}]
+}
